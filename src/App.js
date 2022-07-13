83cbebfd22cc0bf0acf4ccd2e5f5a61fda1c7bd8
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-07-13 15:57:34 -0500

    useRef example 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import UseRefExample1 from "./components/UseRefExample1";
-
+import UseRefExample2 from "./components/useRefExample2";
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 }
